774974a5f4cdc55177be0f83b1135155242fbad6
rexim <reximkut@gmail.com>
2024-07-12 21:00:04 +0700

    [watch] Run tsc once if server.mjs does not exist yet

--- watch.js
+++ watch.js
@@ -1,6 +1,7 @@
 // @ts-check
 // Do not run this file directly. Run it via `npm run watch`. See package.json for more info.
-const { spawn } = require('child_process');
+const { spawn, exit } = require('child_process');
+const fs = require('fs');
 
 /**
  * 
@@ -8,7 +9,7 @@ const { spawn } = require('child_process');
  * @param {string[]} args 
  * @returns {ReturnType<typeof spawn>}
  */
-function cmd(program, args) {
+function cmd(program, args = []) {
     const spawnOptions = { "shell": true };
     console.log('CMD:', program, args.flat(), spawnOptions);
     const p = spawn(program, args.flat(), spawnOptions); // NOTE: flattening the args array enables you to group related arguments for better self-documentation of the running command
@@ -24,7 +25,21 @@ function cmd(program, args) {
     return p;
 }
 
-cmd('tsc', ['-w'])
-// TODO: restart the websocket server when server.mjs is modified
-cmd('node', ['server.mjs'])
-cmd('http-server', ['-p', '6969', '-a', '127.0.0.1', '-s', '-c-1', '-d', 'false'])
+const SERVER_FILE_NAME = 'server.mjs';
+
+function start() {
+    cmd('tsc', ['-w'])
+    // TODO: restart the websocket server when server.mjs is modified
+    cmd('node', [SERVER_FILE_NAME])
+    cmd('http-server', ['-p', '6969', '-a', '127.0.0.1', '-s', '-c-1', '-d', 'false'])
+}
+
+fs.stat(SERVER_FILE_NAME, (err, stat) => {
+    if (err === null) {
+        start();
+    } else if (err.code === 'ENOENT') {
+        cmd('tsc').on('exit', start);
+    } else {
+        throw new Error(`Something went wrong while checking existance of ${SERVER_FILE_NAME}: ${err.code}`);
+    }
+})
